e436466c2932581ffb02ace06a5ffdd763703693
Ching Yi, Chan <chingyichan.tw@gmail.com>
2017-03-11 00:10:42 +0800

    abc

--- app/src/main/java/tw/andyang/multipleflavor/FlavorObject.java
+++ app/src/main/java/tw/andyang/multipleflavor/FlavorObject.java
@@ -1,12 +1,12 @@
 package tw.andyang.multipleflavor;
 
+
 import android.util.Log;
 
 class FlavorObject {
-
     private static final String TAG = "FlavorObject";
 
     void print(){
-        Log.d(TAG, "prod FlavorObject");
+        Log.d(TAG, "mock FlavorObject");
     }
 }
